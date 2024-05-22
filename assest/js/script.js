997605541bf3f6018e7c9f58e55c998487690b1f
Neha <nehakachhawa9@gmail.com>
2024-05-22 19:42:36 +0530

    JS Calculator Work Properly

---
+++ assest/js/script.js
@@ -0,0 +1,106 @@
+let currentValue = "";
+document.querySelector("#display").value =currentValue;
+
+function one(){
+document.getElementById('one');
+currentValue = currentValue + '1';
+document.querySelector("#display").value =currentValue;
+
+}
+function two(){
+    document.getElementById('two');
+    currentValue = currentValue + '2';
+    document.querySelector("#display").value =currentValue;
+}
+function three(){
+    document.getElementById('three');
+    currentValue = currentValue + '3';
+    document.querySelector("#display").value =currentValue;
+}
+function four(){
+    document.getElementById('four');
+    currentValue = currentValue + '4';
+    document.querySelector("#display").value =currentValue;
+}
+function five(){
+    document.getElementById('five');
+    currentValue = currentValue + '5';
+    document.querySelector("#display").value =currentValue;
+}
+function six(){
+    document.getElementById('six');
+    currentValue = currentValue + '6';
+    document.querySelector("#display").value =currentValue;
+}
+function seven(){
+    document.getElementById('seven');
+    currentValue = currentValue + '7';
+    document.querySelector("#display").value =currentValue;
+}
+function eight(){
+    document.getElementById('eight');
+    currentValue = currentValue + '8';
+    document.querySelector("#display").value =currentValue;
+}
+function nine(){
+    document.getElementById('nine');
+    currentValue = currentValue + '9';
+    document.querySelector("#display").value =currentValue;
+}
+function zero(){
+    document.getElementById('zero');
+    currentValue = currentValue + '0';
+    document.querySelector("#display").value =currentValue;
+}
+function zero (){
+    document.getElementById('zero zero');
+    currentValue = currentValue + '00';
+    document.querySelector("#display").value =currentValue;
+}
+function dot(){
+    document.getElementById('dot');
+    currentValue = currentValue + '.';
+    document.querySelector("#display").value =currentValue;
+}
+function clear(){
+    document.getElementById('clear');
+    currentValue = currentValue + 'C';
+    document.querySelector("#display").value =currentValue;
+}
+function add(){
+    document.getElementById('add');
+    currentValue = currentValue + '+';
+    document.querySelector("#display").value =currentValue;
+}
+function sub(){
+    document.getElementById('sub');
+    currentValue = currentValue + '-';
+    document.querySelector("#display").value =currentValue;
+}
+function mult(){
+    document.getElementById('mult');
+    currentValue = currentValue + '*';
+    document.querySelector("#display").value =currentValue;
+}
+function divide(){
+    document.getElementById('divide');
+    currentValue = currentValue + '/';
+    document.querySelector("#display").value =currentValue;
+}
+function equal(){
+    document.getElementById('equal');
+    currentValue = currentValue + '=';
+    document.querySelector("#display").value =currentValue;
+}
+function percentage(){
+    document.getElementById('percentage');
+    currentValue = currentValue + '%';
+    document.querySelector("#display").value =currentValue;
+}
+
+
+
+
+
+
+
